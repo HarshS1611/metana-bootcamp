891dbcc339f582ae69fd14011d6d4959913d4e2b
Harsh Singh <harshsingh16d@gmail.com>
2024-05-05 10:19:10 +0530

    updated sactionedERC20.sol

--- module-1/SanctionedERC20.sol
+++ module-1/SanctionedERC20.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract SanctionedERC20 is ERC20, Ownable {
+contract SanctionedERC20 is ERC20, Ownable2Step {
     mapping(address => bool) public sanctionedAddresses;
 
     constructor(uint256 initialSupply, address initialOwner)
@@ -14,31 +14,27 @@ contract SanctionedERC20 is ERC20, Ownable {
         _mint(msg.sender, initialSupply);
     }
 
-    function addToSanctionList(address account)
-        public
-        onlyOwner
-    {
-        require(!sanctionedAddresses[account], "Sanctioned address");
+    function addToSanctionList(address account) public onlyOwner {
+        require(!sanctionedAddresses[account], "Already sanctioned");
         sanctionedAddresses[account] = true;
     }
 
-    function removeFromSanctionList(address account)
-        public
-        onlyOwner
-    {
-        require(sanctionedAddresses[account], "Not a Sanctioned address");
+    function removeFromSanctionList(address account) public onlyOwner {
+        require(sanctionedAddresses[account], "Not sanctioned");
         sanctionedAddresses[account] = false;
     }
 
-    function authoritativeTransferFrom(
+    function _update(
         address from,
         address to,
-        uint256 amount
-    ) public onlyOwner {
-        require(
-            sanctionedAddresses[from] && sanctionedAddresses[to],
-            "Not a Sanctioned address"
-        );
-        _transfer(from, to, amount);
+        uint256 value
+    ) internal virtual override {
+        super._update(from, to, value);
+        if (from != address(0)) {
+            require(sanctionedAddresses[from], "Not Sanctioned address");
+        }
+        if (from == address(0) || to == address(0)) {
+            require(!sanctionedAddresses[to] || !sanctionedAddresses[from], "Sanctioned address");
+        }
     }
 }
