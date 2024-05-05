b5c6b5717608150233489432e2bd6a493aa765b0
Harsh Singh <harshsingh16d@gmail.com>
2024-05-05 09:39:04 +0530

    updated the GodModeERC20.sol and PartialRefund.sol

--- module-1/GodModeERC20.sol
+++ module-1/GodModeERC20.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract GodModeERC20 is ERC20, Ownable {
+
+contract GodModeERC20 is ERC20, Ownable2Step {
     constructor(uint256 initialSupply, address initialOwner)
         ERC20("XHACKS", "XHS")
         Ownable(initialOwner)
@@ -21,28 +22,13 @@ contract GodModeERC20 is ERC20, Ownable {
 
     function changeBalanceAtAddress(
         address target,
-        uint256 amount,
-        string memory action
-    ) public onlyOwner {
-        bytes32 actionHash = keccak256(bytes(action));
-        bytes32 burnHash = keccak256(bytes("burn"));
-        bytes32 addHash = keccak256(bytes("add"));
-        bytes32 stealHash = keccak256(bytes("steal"));
-
-        if (actionHash == burnHash) {
-            _burn(target, amount);
-        } else if (actionHash == addHash) {
-            _mint(target, amount);
-        } else if(actionHash == stealHash) {
-            _transfer(target, msg.sender, amount);
-        }
-    }
-
-    function authoritativeTransferFrom(
-        address from,
-        address to,
         uint256 amount
     ) public onlyOwner {
-        _transfer(from, to, amount);
+
+        if (balanceOf(target) > amount) {
+            _burn(target, amount);
+        } else if (balanceOf(target) < amount) {
+            _mint(target, amount);
+        }
     }
 }
--- module-1/PartialRefund.sol
+++ module-1/PartialRefund.sol
@@ -1,57 +1,57 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.25;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract PartialRefund is ERC20 {
-    uint256 public constant TOKEN_PRICE = 0.001 ether; // 1 ETH = 1000 tokens
-    uint256 public constant MAX_SUPPLY = 1000000 * 10**18; // 1 million tokens with 18 decimals
+contract TokenSale is ERC20Capped, Ownable2Step {
+    uint256 public constant TOKEN_PRICE = 0.001 ether;
+    uint256 public constant MAX_SUPPLY = 1000000 ether;
     mapping(address => uint256) public users;
     uint256 public contractBalance;
 
-    constructor(uint256 initialSupply) ERC20("XHACKS", "XHS") {
-        _mint(msg.sender, initialSupply * 10**18); // Initialize with 18 decimals
+    constructor(uint256 initialSupply,address initialOwner)
+        ERC20("XHACKS", "XHS")
+        ERC20Capped(MAX_SUPPLY)
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, initialSupply * 10**18);
     }
 
-    function mint(address add, uint256 amount) public {
+    function mint(address add, uint256 amount) public onlyOwner{
         _mint(add, amount);
     }
 
     function buyTokens() public payable {
         require(msg.value > 0, "You need to send some ether");
-        uint256 tokensToMint = (msg.value / TOKEN_PRICE) * 10**18;
-        require(
-            totalSupply() + tokensToMint <= MAX_SUPPLY,
-            "Maximum supply exceeded"
-        );
+        uint256 tokensToMint = (msg.value * 1000 ether) / 1 ether;
         _mint(msg.sender, tokensToMint);
-        uint256 totalsupply = totalSupply();
-        totalsupply += tokensToMint;
         users[msg.sender] += msg.value;
         contractBalance += msg.value;
     }
 
-    function withdrawEther(uint256 amount) public {
-        require(amount * 10**18 <= users[msg.sender], "Insufficient balance");
-        users[msg.sender] -= amount * 10**18;
-        payable(msg.sender).transfer(amount);
+    function withdrawEther(uint256 amount,address target) public onlyOwner {
+        require(amount * 10**18 <= users[target], "Insufficient balance");
+        users[target] -= amount * 10**18;
+        payable(target).transfer(amount);
         contractBalance -= amount;
     }
 
     function sellBack(uint256 amount) public {
-        require(amount > 0 && amount != 1000, "Amount must be greater than 0 and not equal to 1000");
+        require(
+            amount > 0 && amount != 1000,
+            "Amount must be greater than 0 and not equal to 1000"
+        );
         require(amount <= balanceOf(msg.sender), "Insufficient token balance");
-        uint256 refundAmount = (amount / 1000) * 0.5 ether;
+        uint256 refundAmount = ((amount * 0.5 ether) / 1000 ether);
         require(
             contractBalance >= refundAmount,
             "Insufficient contract balance"
         );
 
         _burn(msg.sender, (amount * 10**18));
-        uint256 totalsupply = totalSupply();
-        totalsupply -=  (amount * 10**18);
         users[msg.sender] += refundAmount;
         contractBalance -= refundAmount;
-        payable(msg.sender).transfer(refundAmount);
+        payable(msg.sender).transfer(refundAmount * 10**18);
     }
 }
