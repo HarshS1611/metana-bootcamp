398326cee98b048c9532dc43c26a32d437766c21
Harsh Singh <harshsingh16d@gmail.com>
2024-05-18 12:42:53 +0530

    updated contract to setURI

--- ForgeTokens.sol
+++ ForgeTokens.sol
@@ -2,8 +2,10 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract ForgeToken is ERC1155 {
+contract ForgeToken is ERC1155,Ownable {
     uint256 public constant TOKEN_0 = 0;
     uint256 public constant TOKEN_1 = 1;
     uint256 public constant TOKEN_2 = 2;
@@ -13,8 +15,10 @@ contract ForgeToken is ERC1155 {
     uint256 public constant TOKEN_6 = 7;
 
     constructor()
-        ERC1155("ipfs://QmUhvx74Xtepfoz3RbtftcEiWzaWDP2P3KhNTo8GvMEui4/")
+        ERC1155("https://ipfs.io/ipfs/QmZuFnrvD5qChZuhq4PkDAeKtcFXN3MwMYhwWSKcdk5vW9/{id}")
     {}
+     mapping (uint256 => string) private _uris;
+
 
     function mint(uint256 tokenid, uint256 amount) external {
         require(
@@ -137,4 +141,13 @@ contract ForgeToken is ERC1155 {
         );
         safeTransferFrom(msg.sender, to, tokenid, amount, "");
     }
+
+    function uri(uint256 tokenId) override public view returns (string memory) {
+        return(_uris[tokenId]);
+    }
+    
+    function setTokenUri(uint256 tokenId, string memory uri) virtual public onlyOwner {
+    require(bytes(_uris[tokenId]).length == 0, "Cannot set uri twice");
+    _uris[tokenId] = uri;
+}
 }
