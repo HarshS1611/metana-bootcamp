760d0affea8e743f443486428178f91a6c814818
Harsh Singh <81004813+HarshS1611@users.noreply.github.com>
2024-05-21 12:10:47 +0530

    Update stakeNFT.sol
    updated IERC721Reciever function

--- stakeNFT.sol
+++ stakeNFT.sol
@@ -24,7 +24,7 @@ contract MyNFT is ERC721 {
 
 contract NFTStake is IERC721Receiver  {
     MyToken public token;
-    MyNFT public nft;
+    IERC721 public nft;
 
     struct Stake {
         uint256 tokenId;
@@ -33,9 +33,9 @@ contract NFTStake is IERC721Receiver  {
 
     mapping(address => Stake) public stakes;
 
-    constructor(address _tokenAddress, address _nftAddress) {
+    constructor(address _tokenAddress, IERC721 _nftAddress) {
         token = MyToken(_tokenAddress);
-        nft = MyNFT(_nftAddress);
+        nft = _nftAddress;
     }
     function onERC721Received(
         address operator,
@@ -43,29 +43,26 @@ contract NFTStake is IERC721Receiver  {
         uint256 tokenId,
         bytes calldata data
     ) external returns (bytes4) {
+        require(stakes[msg.sender].tokenId == 0, "You already have an active stake");
+        stakes[from] = Stake(tokenId, block.timestamp);
+
         return IERC721Receiver.onERC721Received.selector;
     }
 
-    function stakeNFT(uint256 tokenId) external {
-        require(nft.ownerOf(tokenId) == msg.sender, "You don't own this NFT");
-        require(stakes[msg.sender].tokenId == 0, "You already have an active stake");
 
-        nft.transferFrom(msg.sender, address(this), tokenId);
-        stakes[msg.sender] = Stake(tokenId, block.timestamp);
-    }
 
     function withdrawNFT() external {
         Stake storage stake = stakes[msg.sender];
         require(stake.tokenId >= 0, "You don't have an active stake");
 
-        nft.transferFrom(address(this), msg.sender, stake.tokenId);
+        nft.safeTransferFrom(address(this), msg.sender, stake.tokenId);
         delete stakes[msg.sender];
     }
 
     function claimReward() external {
         Stake storage stake = stakes[msg.sender];
         require(stake.tokenId >= 0, "You don't have an active stake");
-        require(block.timestamp >= stake.stakingTime + 1 days, "Reward period not over yet");
+        require(block.timestamp >= stake.stakingTime + 25 seconds, "Reward period not over yet");
 
         token.mint(msg.sender, 10 * 1 ether);
         stake.stakingTime = block.timestamp;
