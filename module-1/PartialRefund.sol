87f84aaafc9e0fb067eda02d59554de2b143527c
Harsh Singh <harshsingh16d@gmail.com>
2024-05-23 21:46:10 +0530

    updated partialrefund.sol

--- module-1/PartialRefund.sol
+++ module-1/PartialRefund.sol
@@ -4,21 +4,18 @@ pragma solidity ^0.8.25;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-    contract PartialRefund is ERC20Capped, Ownable2Step {
-    uint256 public constant TOKEN_PRICE = 0.001 ether;
+contract PartialRefund is ERC20Capped, Ownable2Step {
     uint256 public constant MAX_SUPPLY = 1000000 ether;
-    mapping(address => uint256) public users;
     uint256 public contractBalance;
 
-    constructor(uint256 initialSupply,address initialOwner)
-        ERC20("XHACKS", "XHS")
-        ERC20Capped(MAX_SUPPLY)
-        Ownable(initialOwner)
-    {
+    constructor(
+        uint256 initialSupply,
+        address initialOwner
+    ) ERC20("XHACKS", "XHS") ERC20Capped(MAX_SUPPLY) Ownable(initialOwner) {
         _mint(msg.sender, initialSupply * 1 ether);
     }
 
-    function mint(address add, uint256 amount) public onlyOwner{
+    function mint(address add, uint256 amount) public onlyOwner {
         _mint(add, amount);
     }
 
@@ -26,22 +23,16 @@ import "@openzeppelin/contracts/access/Ownable2Step.sol";
         require(msg.value > 0, "You need to send some ether");
         uint256 tokensToMint = (msg.value * 1000 ether) / 1 ether;
         _mint(msg.sender, tokensToMint);
-        users[msg.sender] += msg.value;
         contractBalance += msg.value;
     }
 
-    function withdrawEther(uint256 amount,address target) public onlyOwner {
-        require((amount * 1 ether) <= users[target], "Insufficient balance");
-        users[target] -= amount * 1 ether;
+    function withdrawEther(uint256 amount, address target) public onlyOwner {
+        require(amount <= address(this).balance, "Insufficient balance");
         payable(target).transfer(amount);
         contractBalance -= amount;
     }
 
     function sellBack(uint256 amount) public {
-        require(
-            amount > 0 && amount != 1000,
-            "Amount must be greater than 0 and not equal to 1000"
-        );
         require(amount <= balanceOf(msg.sender), "Insufficient token balance");
         uint256 refundAmount = ((amount * 0.5 ether) / 1000 ether);
         require(
@@ -49,9 +40,8 @@ import "@openzeppelin/contracts/access/Ownable2Step.sol";
             "Insufficient contract balance"
         );
 
-        _burn(msg.sender, (amount * 1 ether));
-        users[msg.sender] += refundAmount;
+        _burn(msg.sender, (amount));
         contractBalance -= refundAmount;
-        payable(msg.sender).transfer(refundAmount * 1 ether);
+        payable(msg.sender).transfer(refundAmount);
     }
 }
