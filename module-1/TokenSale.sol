3365dc3aa2aaae31644519453309073b36f5212b
Harsh Singh <harshsingh16d@gmail.com>
2024-05-23 21:39:53 +0530

    updated tokensale.sol

--- module-1/TokenSale.sol
+++ module-1/TokenSale.sol
@@ -5,30 +5,24 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 contract TokenSale is ERC20Capped, Ownable2Step {
-    uint256 public constant TOKEN_PRICE = 0.001 ether;
-    uint256 public constant MAX_SUPPLY = 1000000 ether; 
-    mapping(address => uint256) public users;
+    uint256 public constant MAX_SUPPLY = 1000000 ether;
 
-     constructor(uint256 initialSupply,address initialOwner)
-        ERC20("XHACKS", "XHS")
-        ERC20Capped(MAX_SUPPLY)
-        Ownable(initialOwner)
-    {
+    constructor(
+        uint256 initialSupply,
+        address initialOwner
+    ) ERC20("XHACKS", "XHS") ERC20Capped(MAX_SUPPLY) Ownable(initialOwner) {
         _mint(msg.sender, initialSupply * 1 ether);
     }
 
     function buyTokens() public payable {
         require(msg.value > 0, "You need to send some ether");
-        uint256 tokensToMint = ((msg.value * 1 ether) / TOKEN_PRICE);
+        uint256 tokensToMint = ((msg.value * 1000 ether) / 1 ether);
         _mint(msg.sender, tokensToMint);
-        uint totalsupply = totalSupply();
-        totalsupply += tokensToMint;
-        users[msg.sender] += msg.value;
+
     }
 
     function withdrawEther(uint256 amount) public onlyOwner {
-        require((amount * 1 ether) <= users[msg.sender], "Insufficient balance");
-        users[msg.sender] -= amount * 1 ether;
-        payable(msg.sender).transfer(amount* 1 ether);
+        require(amount <= address(this).balance, "Insufficient balance");
+        payable(msg.sender).transfer(amount);
     }
 }
