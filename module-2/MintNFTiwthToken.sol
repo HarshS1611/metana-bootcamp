a8c410bdacdf7f1ce4c22c5a203e73c9a2777d4c
Harsh Singh <harshsingh16d@gmail.com>
2024-05-25 22:49:02 +0530

    updated mintNFTwithToken.sol

--- module-2/MintNFTiwthToken.sol
+++ module-2/MintNFTiwthToken.sol
@@ -3,18 +3,40 @@ pragma solidity ^0.8.25;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ERC20Token is ERC20 {
     constructor(uint256 initialSupply) ERC20("XHACKS", "XHS") {
-        _mint(msg.sender, initialSupply);
+        _mint(msg.sender, initialSupply * 1 ether);
+    }
+    function mint(uint256 amount) public {
+        _mint(msg.sender, amount);
     }
 }
 
-contract ERC721Token is ERC721 {
-    constructor() ERC721("MyERC721", "M721") {}
+contract ERC721Token is ERC721, Ownable {
+    address public nftMintContract;
 
-    function mint(address to, uint256 tokenId) external {
-        _mint(to, tokenId);
+    constructor(address initialOwner)
+        ERC721("MyERC721", "M721")
+        Ownable(initialOwner)
+    {}
+
+    modifier onlyNFTMinter() {
+        require(
+            msg.sender == nftMintContract,
+            "Only NFTStake contract can call this function"
+        );
+        _;
+    }
+
+    function setNFTStakeContract(address _nftMintContract) external onlyOwner {
+        require(nftMintContract == address(0), "NFTStake contract already set");
+        nftMintContract = _nftMintContract;
+    }
+
+    function mint(address to, uint256 amount) external onlyNFTMinter {
+        _mint(to, amount);
     }
 }
 
@@ -39,12 +61,12 @@ contract NFTMinter is IERC721Receiver {
 
     function mintNFT(uint256 amount) external {
         require(
-            erc20Token.balanceOf(msg.sender) >= (amount) &&
+            erc20Token.balanceOf(msg.sender) >= (amount * 1 ether) &&
                 amount == 10,
             "Insufficient Balance"
         );
 
-        erc20Token.transferFrom(msg.sender, address(this), amount);
+        erc20Token.transferFrom(msg.sender, address(this), amount * 1 ether);
         erc721Token.mint(msg.sender, tokenid);
         tokenid++;
     }
