2ad8d5ab8ea329821d492de44383778b2f83745c
Harsh Singh <harshsingh16d@gmail.com>
2024-06-04 19:13:12 +0530

    updated stakeNFT.sol

--- module-2/stakeNFT.sol
+++ module-2/stakeNFT.sol
@@ -6,29 +6,12 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/*
-    I have used onlyNFTStake modifier in ERC20Token contract to restrict the mint function to be called only by NFTMinter contract.
-    As it was giving error while using mint or _mint function in NFTStake contract.
-
-    TypeError: Member "mint" not found or not visible after argument-dependent lookup in contract MyToken.
-  --> contracts/stakeNFT.sol:92:9:
-   |
-92 |         token.mint(msg.sender, _rewardAmount);
-   |         ^^^^^^^^^^
-
-
-
-*/
-
 contract MyToken is ERC20, Ownable {
     address public nftStakeContract;
 
-    constructor(uint256 initialSupply, address initialOwner)
-        ERC20("XHACKS", "XHS")
-        Ownable(initialOwner)
-    {
-        _mint(msg.sender, initialSupply);
-    }
+    constructor(
+        address initialOwner
+    ) ERC20("XHACKS", "XHS") Ownable(initialOwner) {}
 
     modifier onlyNFTStake() {
         require(
@@ -54,10 +37,6 @@ contract MyToken is ERC20, Ownable {
 contract MyNFT is ERC721 {
     constructor() ERC721("MyNFT", "MNFT") {}
 
-    /* 
-        I have added mint function to mint NFTs to the users. As its not visible after deplying in the contract.
-    */
-
     function mint(uint256 tokenId) public {
         _mint(msg.sender, tokenId);
     }
@@ -70,10 +49,11 @@ contract NFTStake is IERC721Receiver {
     struct Stake {
         uint256 stakingTime;
         bool isActive;
+        address intialOwner;
     }
     uint256 public constant lockTime = 1 days;
 
-    mapping(address => mapping(uint256 => Stake)) public stakes;
+    mapping(uint256 => Stake) public stakes;
 
     constructor(address _tokenAddress, IERC721 _nftAddress) {
         token = MyToken(_tokenAddress);
@@ -94,35 +74,31 @@ contract NFTStake is IERC721Receiver {
         uint256 tokenId,
         bytes calldata data
     ) external onlyMyNFT returns (bytes4) {
-        Stake storage stake = stakes[from][tokenId];
+        Stake storage stake = stakes[tokenId];
         require(!stake.isActive, "You already have staked");
-        stakes[from][tokenId] = Stake( block.timestamp, true);
+        stakes[tokenId] = Stake(block.timestamp, true, from);
 
         return IERC721Receiver.onERC721Received.selector;
     }
 
-    function withdrawNFT(uint256 tokenId) external {
-        Stake storage stake = stakes[msg.sender][tokenId];
+    function claimLogic (uint256 tokenId) internal {
+        Stake storage stake = stakes[tokenId];
         require(stake.isActive, "You don't have an active stake");
         uint256 elapsedTime = block.timestamp - stake.stakingTime;
+        require(elapsedTime > lockTime, "Cannot claim yet.");
         uint256 _multiplier = elapsedTime / lockTime;
         uint256 _rewardAmount = 10 * _multiplier;
-        token.mint(msg.sender, _rewardAmount);
-        stake.stakingTime = block.timestamp - (elapsedTime % lockTime);
+        token.mint(stake.intialOwner, _rewardAmount);
+        stake.stakingTime += lockTime * _multiplier;
+    }
+
+    function withdrawNFT(uint256 tokenId) external {
+        claimLogic(tokenId);
         nft.safeTransferFrom(address(this), msg.sender, tokenId);
-        delete stakes[msg.sender][tokenId];
+        delete stakes[tokenId];
     }
 
     function claimReward(uint256 tokenId) external {
-        Stake storage stake = stakes[msg.sender][tokenId];
-        uint256 elapsedTime = block.timestamp - stake.stakingTime;
-        require(
-            elapsedTime > lockTime,
-            "Cannot claim yet."
-        );
-        uint256 _multiplier = elapsedTime / lockTime;
-        uint256 _rewardAmount = 10 * _multiplier;
-        token.mint(msg.sender, _rewardAmount);
-        stake.stakingTime = block.timestamp - (elapsedTime % lockTime);
+        claimLogic(tokenId);
     }
 }
