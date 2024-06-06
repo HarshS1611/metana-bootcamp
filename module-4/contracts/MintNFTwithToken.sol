535cf3391ccb81f9d671cb389e40ee8992bb4026
Harsh Singh <harshsingh16d@gmail.com>
2024-06-06 21:18:29 +0530

    updated hadrhat compiler version

--- module-4/contracts/MintNFTwithToken.sol
+++ module-4/contracts/MintNFTwithToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.22;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
