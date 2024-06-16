e0772096e0d6bf5a98cd617b06bf8bbafbc0ce7c
Harsh Singh <harshsingh16d@gmail.com>
2024-06-16 10:26:03 +0530

    updated stakeNFT.sol

--- module-2/stakeNFT.sol
+++ module-2/stakeNFT.sol
@@ -81,24 +81,24 @@ contract NFTStake is IERC721Receiver {
         return IERC721Receiver.onERC721Received.selector;
     }
 
-    function claimToken(uint256 tokenId,bool isClaimable) internal {
+    function claimToken(uint256 tokenId, bool isWithdrawn) internal {
         Stake storage stake = stakes[tokenId];
         require(stake.isActive, "You don't have an active stake");
         uint256 elapsedTime = block.timestamp - stake.stakingTime;
-        require((elapsedTime > lockTime) || isClaimable, "Cannot claim yet.");
+        require((elapsedTime > lockTime) || isWithdrawn, "Cannot claim yet.");
         uint256 _multiplier = elapsedTime / lockTime;
         uint256 _rewardAmount = 10 * _multiplier;
         token.mint(stake.intialOwner, _rewardAmount);
-        toke.stakingTime += lockTime * _multiplier;
+        token.stakingTime += lockTime * _multiplier;
     }
 
     function withdrawNFT(uint256 tokenId) external {
-        claimToken(tokenId,true);
+        claimToken(tokenId, true);
         nft.safeTransferFrom(address(this), msg.sender, tokenId);
         delete stakes[tokenId];
     }
 
     function claimReward(uint256 tokenId) external {
-        claimToken(tokenId,false);
+        claimToken(tokenId, false);
     }
 }
