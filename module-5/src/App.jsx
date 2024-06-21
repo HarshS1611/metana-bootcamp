9fbadf4d76122604ea6eb296fdc1491fb3c3d740
Harsh Singh <harshsingh16d@gmail.com>
2024-06-21 22:07:52 +0530

    got logs of latest block

--- module-5/src/App.jsx
+++ module-5/src/App.jsx
@@ -1,8 +1,9 @@
 
 import VolumeChart from "./VolumeChart";
+import { useState } from "react";
 import { Alchemy, Network } from "alchemy-sdk";
 const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-
+import { ethers } from "ethers";
 const settings = {
   apiKey: "grz0ZmJGLvmh--ZEfeBdhgK2SEEigRg_", // Replace with your Alchemy API Key.
   network: Network.ETH_MAINNET, // Replace with your network.
@@ -10,22 +11,32 @@ const settings = {
 const alchemy = new Alchemy(settings);
 
 const App = () => {
-  const filter = {
-    address: USDC_ADDRESS,
-    topics: [ethers.utils.id("Transfer(address,address,uint256)")],
-  };
-  
-  alchemy.ws.on(filter, (log, event) => {
-    // Emitted whenever a DAI token transfer occurs
-    console.log(log, event);
-  });
-
-  return (
-    <div className="charts">
-      {/* <VolumeChart /> */}
-      hii
-
-    </div>
+  const [blockNumber, setBlockNumber] = useState(0);
+  alchemy.ws.on("block", (blockNumber) => {
+    console.log("Latest block:", blockNumber);
+    setBlockNumber(blockNumber);
+  }
   );
+const filter = {
+  fromBlock: blockNumber - 10,
+  toBlock: blockNumber,
+  address: USDC_ADDRESS,
+  topics: [ethers.id("Transfer(address,address,uint256)")],
+};
+
+alchemy.ws.on(filter, (log, event) => {
+  // Emitted whenever a DAI token transfer occurs
+  console.log(log, event);
+});
+
+
+
+return (
+  <div className="charts">
+    {/* <VolumeChart /> */}
+    hii
+
+  </div>
+);
 }
 export default App;
