c5c36933d12e0e49548e80781b8c8dd7874787dc
Harsh Singh <harshsingh16d@gmail.com>
2024-07-10 15:32:35 +0530

    created merkle tree using bitmap

--- module-8/NFT-whitelist/index.js
+++ module-8/NFT-whitelist/index.js
@@ -1,33 +1,23 @@
-import { MerkleTree } from 'merkletreejs';
-import keccak256  from 'keccak256';
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
 
 let whitelistAdresses = [
-    "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
-    "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",
-    "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",
-    "0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB"
+    ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "0"],
+    ["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", "1"],
+    ["0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db", "2"],
+    ["0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB", "3"]
 ];
 
-let leafNodes = whitelistAdresses.map((address) => keccak256(address));
-let merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+const tree = StandardMerkleTree.of(whitelistAdresses, ["address", "uint256"]);
+console.log("Root hash: ", tree.root);
 
-let rootHash = merkleTree.getHexRoot();
-console.log("Root hash: ", rootHash);
-console.log("Merkle Tree for the whitelist addresses: \n", merkleTree.toString());
-
-
-const claimAddress = "0x17D47f5a76e9601C4179963E18cc17918564864F";
-const claimAddress2 = leafNodes[0];
-
-let proof = merkleTree.getHexProof(claimAddress);
-let proof2 = merkleTree.getHexProof(claimAddress2);
-
-console.log("Proof for address 0x17D47f5a76e9601C4179963E18cc17918564864F: \n", proof);
-console.log("Proof for address 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4: \n", proof2);
-
-let isValid = merkleTree.verify(proof, claimAddress, rootHash);
-let isValid2 = merkleTree.verify(proof2, claimAddress2, rootHash);
-
-
-console.log("Is address 0x17D47f5a76e9601C4179963E18cc17918564864F in the whitelist? ", isValid);
-console.log("Is address 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4 in the whitelist? ", isValid2);
+for (const [i, v] of tree.entries()) {
+    if (v[0] === "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4") {
+      const proof = tree.getProof(i);
+      console.log("Value:", v);
+      console.log("Proof:", proof);
+    }
+  }
+  [
+    "0x94b29c01ed483e694a7ecf386d384987d4d3e9d4e6c476f5b97302b23ff871c9",
+    "0xfbd9dfd10273074214b15a83e871923fa9e88f9abd03ab2a7891a42701e72639"
+  ]
