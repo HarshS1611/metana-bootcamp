6e70d78677eb507c0db2b88aef1505554fbca66a
Harsh Singh <harshsingh16d@gmail.com>
2024-07-20 18:02:07 +0530

    v1 deployed

--- module-9/deploy/nftV1.js
+++ module-9/deploy/nftV1.js
@@ -2,10 +2,8 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
 
-    [owner] = await ethers.getSigners();
-
     const nftV1 = await ethers.getContractFactory("MyNFTV1");
-    const nftV1Contract = await upgrades.deployProxy(nftV1, [owner.address], { initializer: 'initialize' });
+    const nftV1Contract = await upgrades.deployProxy(nftV1, [], { initializer: 'initialize' });
     await nftV1Contract.waitForDeployment();
 
     console.log("MyNFT deployed to:", nftV1Contract.target);
