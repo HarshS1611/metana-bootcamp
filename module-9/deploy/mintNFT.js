463759233e1fecb3e42677fc7e260dbbe56aa95a
Harsh Singh <harshsingh16d@gmail.com>
2024-07-20 19:31:24 +0530

    upgraded mint nft contract

--- module-9/deploy/mintNFT.js
+++ module-9/deploy/mintNFT.js
@@ -5,25 +5,21 @@ async function main() {
 
     console.log("Deploying contracts with the account:", deployer.address);
 
-    // Deploy ERC20TokenUpgradeable
     const ERC20Token = await ethers.getContractFactory("ERC20Token");
     const erc20Token = await upgrades.deployProxy(ERC20Token, [], { initializer: 'initialize' });
-    await erc20Token.deployed();
+    await erc20Token.waitForDeployment();
     console.log("ERC20TokenUpgradeable deployed to:", erc20Token.target);
 
-    // Deploy ERC721TokenUpgradeable
     const ERC721Token = await ethers.getContractFactory("ERC721Token");
     const erc721Token = await upgrades.deployProxy(ERC721Token, [deployer.address], { initializer: 'initialize' });
-    await erc721Token.deployed();
+    await erc721Token.waitForDeployment();
     console.log("ERC721TokenUpgradeable deployed to:", erc721Token.target);
 
-    // Deploy NFTMinterUpgradeable
     const NFTMinter = await ethers.getContractFactory("NFTMinter");
     const nftMinter = await upgrades.deployProxy(NFTMinter, [erc20Token.target, erc721Token.target, deployer.address], { initializer: 'initialize' });
-    await nftMinter.deployed();
+    await nftMinter.waitForDeployment();
     console.log("NFTMinterUpgradeable deployed to:", nftMinter.target);
 
-    // Set NFTMinter contract address in ERC721Token
     await erc721Token.setNFTMintContract(nftMinter.target);
     console.log("NFTMinter contract address set in ERC721Token");
 }
